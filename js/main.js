338fa2a743a8643957c9f8481a4b90812394449d
Oleg <clocico.oleg19@gmail.com>
2023-03-01 23:29:04 +0200

    Task1

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let age = prompt('Введите число?');
+switch (age) {
+    case "1":
+      alert( 'Один' );
+      break;
+    case "2":
+      alert( 'Два' );
+      break;
+    case "3":
+      alert( 'Три' );
+      break;
+    case "4":
+      alert( 'Четыре' );
+      break;
+    case "5":
+      alert( 'Пять' );
+      break;
+    case "6":
+      alert( 'Шесть' );
+      break;
+     case "7":
+      alert( 'Семь' );
+      break;
+    case "8":
+      alert( 'Восемь' );
+      break;
+    case "9":
+      alert( 'Девять' );
+      break;
+    
+  }
