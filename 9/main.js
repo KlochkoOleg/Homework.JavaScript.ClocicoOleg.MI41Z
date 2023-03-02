05fceda910e721a2ee527f12fb71915f0e0989d4
Oleg <clocico.oleg19@gmail.com>
2023-03-02 12:22:33 +0200

    Task9

---
+++ 9/main.js
@@ -0,0 +1,7 @@
+let name = prompt('Введите Имя :' );
+alert(`Привет ${name} !`);
+result = confirm(`Знаком с языком JavaScript?`);
+if(result==true){
+alert(`Молодец!`);
+}else
+alert(`Начни изучение сейчас!`);
