a34def5432902fcea604c6ac3adeac6ee7a0edfc
Oleg <clocico.oleg19@gmail.com>
2023-03-05 19:58:12 +0200

    Task10

---
+++ 1/js/main.js
@@ -0,0 +1,9 @@
+let a = prompt('Введите начало интервала');
+let b = prompt('Введите конец интервала');
+var s=0;
+function sum ()
+for(a; a<=b; a++){
+ if(a%2==0)
+   s+=i;
+  }
+  console.log(s);
--- 3/main.js
+++ 3/main.js
@@ -1,5 +1,5 @@
-let c = prompt("Введите первое число:")
+let c = prompt("Введите имя:")
 let a = prompt("Введите второе число");
 let b = prompt("Введите третье число");
- console.log('Наименьшее число: ' + Math.min(a, b, c) +'Наибольшее число: ' + Math.max(a, b, c));
+ console.log('Наименьшее число: ' + Math.min(a, b, c) +' Наибольшее число: ' + Math.max(a, b, c));
 
